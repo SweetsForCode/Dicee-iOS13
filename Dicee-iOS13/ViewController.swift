39075008822fb89e0e00b8797ed06995235cd4e4
Charlie Nguyen <CharlieHN97@gmail.com>
2019-06-14 10:23:35 +0100

    Add missing SceneDelegate

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: IBOutlet(s)
+    @IBOutlet weak var rollDiceButton: UIButton!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
 
+    var diceImageArray: [UIImage] = []
+    
+    //MARK: viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        rollDiceButton.layer.cornerRadius = 5
+        diceImageArray = [UIImage(imageLiteralResourceName: "DiceOne"),
+                          UIImage(imageLiteralResourceName: "DiceTwo"),
+                          UIImage(imageLiteralResourceName: "DiceThree"),
+                          UIImage(imageLiteralResourceName: "DiceFour"),
+                          UIImage(imageLiteralResourceName: "DiceFive"),
+                          UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceImageViewOne.image = UIImage.init(imageLiteralResourceName: "DiceOne")
+        diceImageViewTwo.image = UIImage.init(imageLiteralResourceName: "DiceOne")
+        
     }
 
-
+    //MARK: IBAction(s)
+    @IBAction func rollDicePressed(_ sender: Any) {
+        diceImageViewOne.image = diceImageArray[getRandomNum()]
+        diceImageViewTwo.image = diceImageArray[getRandomNum()]
+    }
+    
+    func getRandomNum() -> Int {
+        return Int.random(in: 0...5)
+    }
+ 
 }
 
